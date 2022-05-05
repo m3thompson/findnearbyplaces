67a6dac26b70f422c3482181d9119c7a05f89e45
MITCHELL THOMPSON <MTHOMPSON3@email.arizona.edu>
2022-05-05 15:08:11 -0700

    15:07

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+// the next 2 imports were needed for the commented code at the bottom
+// import logo from './logo.svg';
+// import './App.css';
+
+import { Navigate, HashRouter, Routes, Route, useParams } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+import FederatedLogin from './components/FederatedLogin';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Menu from './components/Menu';
+import Quiz from './components/Quiz';
+import { useState } from 'react';
 
 function App() {
+  const [customer, setCustomer] = useState(localStorage.getItem('customer'));
+
+  let customerLoggedInHandler = (customerEmail) => {
+    localStorage.setItem('customer', customerEmail);
+    setCustomer(customerEmail);
+  }
+
+  let customerLoggedOutHandler = () => {
+    localStorage.removeItem('customer');
+    setCustomer(undefined);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Container fluid>
+        <Row>
+          <Col>
+            <Header />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Menu customer={customer} customerLoggedOut={customerLoggedOutHandler} />
+          </Col>
+        </Row>
+
+
+        <Routes>
+
+          <Route exact path='/register' element={<Register />}>
+          </Route>
+
+          <Route exact path='/login/:from' element={<Login customerLoggedIn={customerLoggedInHandler} />}>
+
+          </Route>
+
+          <Route exact path='/login' element={<Login customerLoggedIn={customerLoggedInHandler} />}>
+
+          </Route>
+
+          <Route path='/google/:username/:name' element={<FederatedLogin provider="google" customerLoggedIn={customerLoggedInHandler} />}>
+
+          </Route>
+
+          <Route exact path='/quiz/:id' element={
+            <ProtectedRoute customer={customer}><Quiz /></ProtectedRoute>
+
+          }>
+
+          </Route>
+          <Route exact path='/' element={<Home />}>
+
+          </Route>
+        </Routes>
+
+        <Row>
+          <Col>
+            <Footer />
+          </Col>
+        </Row>
+
+      </Container>
+    </HashRouter>
   );
+}
+
+const ProtectedRoute = ({ customer, children }) => {
+  const { id } = useParams();
+  
+  if (customer) {
+    return children;
+  } else {
+    return <Navigate to={`/login/${id}`} />;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+/*
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,5 +35,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-  //
+*/
